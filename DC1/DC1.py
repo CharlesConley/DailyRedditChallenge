b5caa48b4453b17766cf6bd9563a26bb7bd00168
CharlesConley <ccconley2@wisc.edu>
2017-10-26 10:35:39 -0500

    DailyChalenge1
    This is the first iteration of daily challenge 1 that does the minimal input and output.

---
+++ DC1/DC1.py
@@ -0,0 +1,12 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+# This is the first challenge and the first iteration of it. Next iteration will attempt to use a GUI to avoid stdin
+# 10/26/2017
+
+test = "hello world!"
+
+print(test)
+name = input("Hello User, what is your name? ")
+age = input("Hello " + name + " what is your age? ")
+username = input("Hello " + name + " what is your reddit Username? ")
+
+print(name + " " + age + " " + username + " ")
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DailyRedditChallenge
+
+This folder will contain daily reddit coding challenges from https://www.reddit.com/r/dailyprogrammer/wiki/challenges
+The main goal of this will be to learn new languages through unique tasks.
+
+The index will contain the challenge ID and the language(s) used to achieve the challenge.
+Index:
